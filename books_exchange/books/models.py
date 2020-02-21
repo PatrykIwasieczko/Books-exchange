743064e8a504b20e5ae913aa452998e51ed7d574
Patryk <patryk.iwasieczko@gmail.com>
2020-02-21 21:24:19 +0100

    Add user phone number and books categories

--- books_exchange/books/models.py
+++ books_exchange/books/models.py
@@ -5,12 +5,24 @@ from django.urls import reverse
 
 
 class Book(models.Model):
+    GENRE_CHOICES = (
+        ('action', 'Action and Adventure'),
+        ('crime', 'Crime'),
+        ('fantasy', 'Fantasy'),
+        ('horror', 'Horror'),
+        ('romance', 'Romance'),
+        ('sci-fi', 'Science Fiction'),
+        ('thriller', 'Thriller')
+    )
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.CharField(max_length=100)
     owner = models.ForeignKey(
         User, on_delete=models.CASCADE, blank=True, null=True)
+    category = models.CharField(
+        max_length=20, choices=GENRE_CHOICES, blank=True, null=True)
 
     def __str__(self):
         return self.title
